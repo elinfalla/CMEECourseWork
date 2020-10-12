9cd2317340f953773e878b65b2e8130ba72f9c56
Elin Falla <elinfalla@me.com>
2020-10-12 15:05:46 +0100

    Modified TabtoCsv.sh so it can intake any file extension and convert to csv, not just .txt

--- Week1/Code/Tabtocsv.sh
+++ Week1/Code/Tabtocsv.sh
@@ -11,19 +11,24 @@
 # Date: Oct 2020
 
 if [[ ! -f $1 ]];
-    then echo "No file given. Please give tab delimted file as argument."; 
+    then echo "No file given. Please give tab delimted file as argument.";
     exit 1
 fi
 
 if [[ -f $2 ]];
-    then echo "Please give only one tab delimted file as an argument."; 
+    then echo "Please give only one tab delimted file as an argument.";
     exit 2
 fi
 
 echo "Creating a comma delimited version of $1 ..."
 
+# Use string manipulations to extract base name of file (no extension)
+path=$1 #$1 is the filepath of the inputted file
+file=${path##*/} # ${variable%%pattern} trims the longest match from start of string (#-shortest) so */ removes all directories, leaving just the filename and extension
+base=${file%%.*} # ${variable%%pattern} trims the longest match from end of string (%=shortest) so .* removes any extension
+
 # Substitute tabs for commas then transfer it to a file with a csv extention
-cat $1 | tr -s "\t" "," >> $(basename "$1" .txt).csv #basename removes extensions - in this case txt
+cat $1 | tr -s "\t" "," >> $base.csv
 echo "Done!"
 
 exit 0
