654876816822b6071abde7d25ab4117f33e73c7b
Elin Falla <elinfalla@me.com>
2020-10-22 21:36:50 +0100

    Finish implementing in-class R scripts

---
+++ Week3/Code/Ricker.R
@@ -0,0 +1,19 @@
+#!/usr/bin/env R
+
+# delete everything
+rm(list=ls(all=TRUE))
+
+Ricker <- function(N0=1, r=1, K=10, generations=50) {
+  # Runs a simulation of the Ricker model
+  # Returns a vector of length generations
+  
+  N <- rep(NA, generations) #Creates vector of NA
+  
+  N[1] <- N0
+  for (t in 2:generations) {
+    N[t] <- N[t-1] * exp(r*(1.0-(N[t-1]/K)))
+  }
+  return(N)
+}
+
+plot(Ricker(generations = 10), type = "l")
