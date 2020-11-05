d6ec7007fb2bd24a656455f5130a2116ef2c503d
Elin Falla <elinfalla@me.com>
2020-11-05 22:33:24 +0000

    Modified .gitignore to include *.tex.bak files

--- Week3/Code/tmp.R
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env R
-
-MyData <- as.matrix(read.csv("../Data/PoundHillData.csv", header=F, stringsAsFactors=F))
-class(MyData)
-
-MyMetaData <- read.csv("../Data/PoundHillMetaData.csv", header=T, sep=";", stringsAsFactors=F)
-class(MyMetaData)
-
-head(MyData)
